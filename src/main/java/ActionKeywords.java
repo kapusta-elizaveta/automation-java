3f09854214f0c226ae7bca5d9fc8dd168faa6fcc
Lizaveta Kapusta <kapustaeliz@gmail.com>
2021-03-02 17:52:24 +0300

    add dataengine

---
+++ src/main/java/ActionKeywords.java
@@ -0,0 +1,47 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class ActionKeywords {
+
+    public static WebDriver driver;
+
+    public static void openBrowser(){
+        driver=new ChromeDriver();
+    }
+
+    public static void navigate(){
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://www.store.demoqa.com");
+    }
+
+    public static void click_MyAccount(){
+        driver.findElement(By.xpath(".//*[@id='account']/a")).click();
+    }
+
+    public static void input_Username(){
+        driver.findElement(By.id("log")).sendKeys("testuser_3");
+    }
+
+    public static void input_Password(){
+        driver.findElement(By.id("pwd")).sendKeys("Test@123");
+    }
+
+    public static void click_Login(){
+        driver.findElement(By.id("login")).click();
+    }
+
+    public static void waitFor() throws Exception{
+        Thread.sleep(5000);
+    }
+
+    public static void click_Logout(){
+        driver.findElement (By.xpath(".//*[@id='account_logout']/a")).click();
+    }
+
+    public static void closeBrowser(){
+        driver.quit();
+    }
+}
